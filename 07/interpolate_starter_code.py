5f875ea435a33277b87fe704024e86852840a58e
MankiratMann <mankirat.mann@ontariotechu.net>
2024-03-11 15:57:55 -0400

    lab 7 done

--- 07/interpolate_starter_code.py
+++ 07/interpolate_starter_code.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def intpl(x, y):
-    n = len(x) - 1  # Extract the polynomial order of interpolation.
-    V = np.vander(x, increasing=True)  # Vandermonde matrix.
-    c = np.linalg.cond(V, 2)  # Condition number of V.
-    a = np.linalg.solve(V, y)  # Solve for the coefficients.
+    n = len(x) - 1
+    V = np.vander(x, increasing=True)
+    c = np.linalg.cond(V, 2)
+    a = np.linalg.solve(V, y)
     return a, c
